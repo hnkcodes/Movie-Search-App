a8afc97e8187bbe310d1fdee017ba4fe6ae61fbe
hnk <217316986+hnkcodes@users.noreply.github.com>
2026-08-11 19:09:43 +0900

    Move API key to environment variable

--- src/util/api-request-options.js
+++ src/util/api-request-options.js
@@ -1,8 +1,9 @@
+const API_KEY = import.meta.env.TMDB_API_KEY;
+
 export const options = {
   method: "GET",
   headers: {
     accept: "application/json",
-    Authorization:
-      "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI3ZDljY2E1M2Q0ZmYzY2FhYWQwY2M3Yzk5ZDA2N2VmZSIsIm5iZiI6MTc4NDcyMzkzOS4zNDQsInN1YiI6IjZhNjBiOWUzODQwNWNkNWRjOTcxODY5MyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.mmKfhYFo1PFW2fKL3c9_FEOjH2FFM3XKBwsHIejbLxo",
+    Authorization: `Bearer ${API_KEY}`,
   },
 };
